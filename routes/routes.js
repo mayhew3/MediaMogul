b374c2e0c9fee073141167e99463e9ef3ef0d1c9
Mayhew Seavey <scorpy@gmail.com>
2014-11-13 15:05:17 -0800

    Split out routes into separate file

---
+++ routes/routes.js
@@ -0,0 +1,34 @@
+module.exports = function(app) {
+    var shows = require('../controllers/shows_controller');
+    app.get('/', function (req, res) {
+        res.render('index', {title: "ShowList"});
+    });
+    app.get('/notes', function (req, res) {
+        res.render('angTest');
+    });
+    app.get('/shows', shows.getShows);
+
+    // error handlers
+
+    // development error handler
+    // will print stacktrace
+    if (app.get('env') === 'development') {
+        app.use(function (err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
+        });
+    }
+
+    // production error handler
+    // no stacktraces leaked to user
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
+};
