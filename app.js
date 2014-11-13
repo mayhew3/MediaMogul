bb6979c39c86b8dd487f1ec43db0353182044c99
Mayhew Seavey <scorpy@gmail.com>
2014-11-12 16:33:40 -0800

    Split out app server start like it started.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var debug = require('debug')('MediaMogul');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -62,9 +61,3 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function() {
-    debug('Express server listening on port ' + server.address().port);
-});
