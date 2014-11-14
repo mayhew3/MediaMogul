aa3edb4a9b4c1948edb91ea8fc908f2c62b14270
Mayhew Seavey <scorpy@gmail.com>
2014-11-13 17:55:30 -0800

    It works!!! Well, it commits to the DB when the checkbox is CHECKED, but not unchecked. But still.

--- public/javascripts/angular_app.js
+++ public/javascripts/angular_app.js
@@ -1,17 +1,18 @@
 angular.module('mediaMogulApp', [])
     .controller('showController', ['$scope', '$http',
         function($scope, $http) {
-    $http.get('/shows')
-        .success(function(data, status, headers, config) {
-            $scope.shows = data;
-            $scope.thingsHappened = "Nope.";
-            $scope.error = "";
-        }).error(function(data, status, headers, config) {
-            $scope.shows = {};
-            $scope.error = data;
-        });
+            $http.get('/shows')
+                .success(function(data, status, headers, config) {
+                    $scope.shows = data;
+                    $scope.thingsHappened = "Nope.";
+                    $scope.error = "";
+                }).error(function(data, status, headers, config) {
+                    $scope.shows = {};
+                    $scope.error = data;
+                });
             $scope.change = function(show) {
                 //debug("User clicked.");
                 $scope.thingsHappened = show.Title;
+                $http.post('/markWatched', {episodeId: show._id});
             }
-}]);
+        }]);
