5d505f8d9357bc07ba72683794da7535bc923fb1
Mayhew Seavey <scorpy@gmail.com>
2014-12-05 21:07:09 -0800

    Pulled out a new ShowService that can be re-used. Not sure if I have the then() chaining completely right, but it works. NOTE: It wasn't working until I put in the then() call, because it was calling getShowList() before the HTTP request had finished and set the "shows" variable. Timing is a bit tricky to get right.

--- public/javascripts/angular_app.js
+++ public/javascripts/angular_app.js
@@ -1,18 +1,43 @@
+function ShowService($log, $http) {
+    var shows = [];
+    var error = "";
+
+    this.updateShowList = function() {
+        return $http.get('/shows')
+            .success(function(data, status, headers, config) {
+                $log.debug(data[0]);
+                shows = data;
+                error = "";
+            }).error(function(data, status, headers, config) {
+                $log.debug("Error!");
+                shows = [];
+                error = data;
+            });
+    };
+    this.getShowList = function() {
+        $log.debug("Getting show list, size " + shows.length);
+        return shows;
+    };
+    this.getError = function() {
+        return error;
+    };
+    this.markWatched = function(episodeId, watched) {
+        $http.post('/markWatched', {episodeId: episodeId, watched: watched});
+        // todo: add some error handling.
+    }
+}
 angular.module('mediaMogulApp', [])
-    .controller('showController', ['$http',
-        function($http) {
+    .service('ShowService', ['$log', '$http', ShowService])
+    .controller('showController', ['ShowService',
+        function(ShowService) {
             var self = this;
 
-            $http.get('/shows')
-                .success(function(data, status, headers, config) {
-                    self.shows = data;
-                    self.error = "";
-                }).error(function(data, status, headers, config) {
-                    self.shows = {};
-                    self.error = data;
-                });
+            ShowService.updateShowList().then(function(updateResponse) {
+                self.shows = ShowService.getShowList();
+                self.error = ShowService.getError();
+            });
+
             self.change = function(show) {
-                $http.post('/markWatched', {episodeId: show._id, watched: show.Watched});
-                // todo: add some error handling.
+                ShowService.markWatched(show._id, show.Watched);
             };
         }]);
