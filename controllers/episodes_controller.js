46d44ebc8df84348e33350656527047038f36c4f
Mayhew Seavey <scorpy@gmail.com>
2014-12-08 17:17:50 -0800

    Rename part 2. Git couldn't figure out the rename and big edit simultaneously.

--- controllers/episodes_controller.js
+++ controllers/episodes_controller.js
@@ -1,17 +1,17 @@
 var mongoose = require('mongoose'),
-    Show = mongoose.model('episodes');
-exports.getShows = function(req, res) {
-    Show.find({Suggestion:false, DeletedDate: {"$exists" : false}}).sort({ShowingStartTime:-1}).limit(100)
-        .exec(function(err, shows) {
-            if (!shows) {
-                res.json(404, {msg: 'Shows Not Found.'});
-            } else {
-                res.json(shows);
-            }
-        });
+  Episode = mongoose.model('episodes');
+exports.getEpisodes = function(req, res) {
+    Episode.find({Suggestion:false, DeletedDate: {"$exists" : false}}).sort({ShowingStartTime:-1}).limit(100)
+      .exec(function(err, episodes) {
+          if (!episodes) {
+              res.json(404, {msg: 'Episodes Not Found.'});
+          } else {
+              res.json(episodes);
+          }
+      });
 };
-exports.markShowAsWatched = function(req, res) {
-  Show.update({_id: req.body.episodeId}, {$set:{Watched:req.body.watched}})
+exports.markEpisodeAsWatched = function(req, res) {
+    Episode.update({_id: req.body.episodeId}, {$set:{Watched:req.body.watched}})
       .exec(function(err, savedEpisode) {
           if (err) {
               res.json(404, {msg: 'Failed to update Episode as watched.'});
