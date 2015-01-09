c34e9db273037a13086c39088c42aadfbac14bf4
Mayhew Seavey <scorpy@gmail.com>
2015-01-08 21:51:15 -0800

    Plugged in the TVDB code and now Adding Show pulls in the external data (not episodes yet) and commits to database.

--- controllers/series_controller.js
+++ controllers/series_controller.js
@@ -23,18 +23,102 @@ exports.changeTier = function(req, res) {
           }
       });
 };
-exports.addSeries = function(req, res) {
-    var seriesObj = req.body.series;
+exports.addSeries = function(req, res, next) {
+  var seriesObj = req.body.series;
   console.log("Series info: " + JSON.stringify(seriesObj));
   var series = new Series(seriesObj);
   console.log("Full Series: " + JSON.stringify(series));
-  series.save(function(err) {
-    if (err) {
-      res.json(404, {msg: 'Failed to insert Series.'});
-    } else {
-      res.json({msg: "success"});
-    }
+
+  var seriesName = series.SeriesTitle
+    .toLowerCase()
+    .replace(/ /g, '_')
+    .replace(/[^\w-]+/g, '');
+  var apiKey = '04DBA547465DC136';
+
+  console.log("Entering start of method for series " + seriesName);
+
+  var parser = xml2js.Parser({
+    explicitArray: false,
+    normalizeTags: true
   });
+
+  async.waterfall([
+    function(callback) {
+      console.log("In first callback.");
+      request.get('http://thetvdb.com/api/GetSeries.php?seriesname=' + seriesName, function (error, response, body) {
+        if (error) return next(error);
+        console.log("Made successful first API call.");
+        parser.parseString(body, function(err, result) {
+          if (!result.data.series) {
+            return res.send(400, { message: seriesName + ' was not found.' });
+          }
+          var tvdbId = result.data.series.seriesid || result.data.series[0].seriesid;
+          console.log("Found ID on TVDB! Id is " + tvdbId);
+          callback(err, series, tvdbId);
+        });
+      });
+    },
+    function (existingSeries, tvdbId, callback) {
+      console.log("In second callback.");
+      request.get('http://thetvdb.com/api/' + apiKey + '/series/' + tvdbId + '/all/en.xml', function (error, response, body) {
+        if (error) return next(error);
+        console.log("Made successful second API call.");
+        parser.parseString(body, function (err, result) {
+          var series = result.data.series;
+          var episodes = result.data.episode;
+
+          existingSeries.tvdbId = series.id;
+          existingSeries.tvdbName = series.seriesname;
+          existingSeries.airsDayOfWeek = series.airs_dayofweek;
+          existingSeries.airsTime = series.airs_time;
+          existingSeries.firstAired = series.firstaired;
+          existingSeries.genre = series.genre.split('|').filter(Boolean);
+          existingSeries.network = series.network;
+          existingSeries.overview = series.overview;
+          existingSeries.rating = series.rating;
+          existingSeries.ratingCount = series.ratingcount;
+          existingSeries.runtime = series.runtime;
+          existingSeries.status = series.status;
+          existingSeries.poster = series.poster;
+
+          console.log("Show data: " + JSON.stringify(existingSeries));
+          console.log("Episode data: " + episodes);
+          callback(err, existingSeries);
+        });
+      });
+    },
+    function (series, callback) {
+      console.log("In third callback.");
+      var url = 'http://thetvdb.com/banners/' + series.poster;
+      request({ url: url, encoding: null }, function (error, response, body) {
+        console.log("Made successful third API call.");
+        series.poster = 'data:' + response.headers['content-type'] + ';base64,' + body.toString('base64');
+        callback(error, series);
+      });
+    }
+  ], function (err, series) {
+    console.log("In final callback.");
+    if (err) return next(err);
+
+    console.log("Data found for series: " + JSON.stringify(series));
+
+
+    series.save(function(err) {
+      if (err) {
+        res.json(404, {msg: 'Failed to insert Series.'});
+      } else {
+        res.json({msg: "success"});
+      }
+    });
+
+
+    /*
+    res.json({msg: "success"});
+    */
+
+  });
+
+
 };
 exports.updateSeries = function(req, res) {
   Series.update({_id: req.body.SeriesId}, req.body.ChangedFields)
