9104763b53d77cff027537ef21783652158dacf2
Mayhew Seavey <commander.boffo@gmail.com>
2018-10-26 22:52:40 -0700

    MM-541: Add new game card directive.

---
+++ public/javascripts/games/gameCardController.js
@@ -0,0 +1,35 @@
+(function () {
+  'use strict';
+
+  angular.module('mediaMogulApp')
+    .directive('gameCard', gameCard);
+
+  function gameCard() {
+    return {
+      templateUrl: 'views/games/gameCard.html',
+      controller: gameCardController,
+      controllerAs: 'ctrl'
+    }
+  }
+
+  function gameCardController($uibModal, $scope) {
+    var self = this;
+
+    self.game = $scope.game;
+
+    self.open = function(game) {
+      $uibModal.open({
+        templateUrl: 'views/games/gameDetail.html',
+        controller: 'gameDetailController as ctrl',
+        size: 'lg',
+        resolve: {
+          game: function() {
+            return game;
+          }
+        }
+      });
+    };
+
+  }
+
+})();
