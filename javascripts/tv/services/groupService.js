87e525ccbb8d85c02c5091ea2871dfbcc647fb1f
mayhew3 <commander.boffo@gmail.com>
2019-04-19 00:02:50 -0700

    MM-784: Intermediate checkin client side

---
+++ javascripts/tv/services/groupService.js
@@ -0,0 +1,9 @@
+angular.module('mediaMogulApp')
+    .service('GroupService', ['$log', '$http', 'ArrayService',
+      function ($log, $http, ArrayService) {
+        const self = this;
+
+        self.getGroupSeries = function(series, tv_group_id) {
+          return _.findWhere(series.groups, {tv_group_id: tv_group_id});
+        };
+      }]);
