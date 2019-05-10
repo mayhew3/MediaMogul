e75a07fc941a637757ed1465ac5398730e429ba3
Mayhew Seavey <commander.boffo@gmail.com>
2019-05-10 09:27:43 -0700

    MM-876: Add client lists for each person_id

--- controllers/sockets_controller.js
+++ controllers/sockets_controller.js
@@ -1,6 +1,9 @@
 const arrayService = require('../controllers/array_util');
+const _ = require('underscore');
 
 const clients = [];
+const persons = [];
+
 let io;
 
 exports.initIO = function(in_io) {
@@ -9,14 +12,37 @@ exports.initIO = function(in_io) {
     console.log('Connection established. Adding client.');
     clients.push(client);
 
-    let person_id = client.handshake.query.person_id;
+    let person_id = parseInt(client.handshake.query.person_id);
+    addClientForPerson(person_id, client);
 
     client.on('disconnect', () => {
       console.log('Client disconnected. Removing from array.');
       arrayService.removeFromArray(clients, client);
+      removeClientForPerson(person_id, client);
     });
   });
 };
+
+function addClientForPerson(person_id, client) {
+  const existingArray = _.findWhere(persons, {person_id: person_id});
+  if (!existingArray) {
+    persons.push({
+      person_id: person_id,
+      clients: [client]
+    });
+  } else {
+    existingArray.clients.push(client);
+  }
+}
+
+function removeClientForPerson(person_id, client) {
+  const existingArray = _.findWhere(persons, {person_id: person_id});
+  if (!existingArray) {
+    console.log("Warning: Disconnect received for person_id that never connected: " + person_id);
+  } else {
+    arrayService.removeFromArray(existingArray.clients, client);
+  }
+}
 
 exports.getNumberOfClients = function() {
   return clients.length;
