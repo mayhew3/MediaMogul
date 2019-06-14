8ec19b1136593cacb0ab51251d3eb7bdc593ee4a
Mayhew Seavey <commander.boffo@gmail.com>
2019-06-13 17:34:58 -0700

    MM-837: Compile full and minified css file. Rename from app to bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,21 @@ module.exports = function(grunt) {
   grunt.initConfig({
     clean: [ "dist" ],
     less: {
-      production: {
+      dist: {
         options: {
           paths: [ "node_modules/bootstrap/less" ],
-          cleancss: true
+          compress: false,
+          strictMath: true
         },
         files: {
-          "stylesheets/dist/css/app.min.css": "stylesheets/less/app.less"
+          "stylesheets/dist/css/bootstrap.css": "stylesheets/less/bootstrap.less"
         }
       }
     },
     uglify: {
       bootstrap: {
         files: {
-          "stylesheets/dist/js/app.min.js": [
+          "stylesheets/dist/js/bootstrap.min.js": [
             "node_modules/bootstrap/js/transition.js",
             "node_modules/bootstrap/js/alert.js",
             "node_modules/bootstrap/js/button.js",
@@ -44,17 +45,33 @@ module.exports = function(grunt) {
         files: [
           "less/*.less"
         ],
-        tasks: [ "less" ]
+        tasks: [ "less:dist" ]
       }
     },
   });
-  
+
+  grunt.registerTask('compress', 'compress a generic css', function(fileSrc, fileDst) {
+    const files = {};
+    files[fileDst] = fileSrc;
+    grunt.log.writeln('compressing file ' + fileSrc);
+
+    grunt.config('less.dist.files', files);
+    grunt.config('less.dist.options.compress', true);
+    grunt.task.run(['less:dist']);
+  });
+
   grunt.loadNpmTasks("grunt-contrib-clean");
   grunt.loadNpmTasks("grunt-contrib-less");
   grunt.loadNpmTasks("grunt-contrib-uglify");
   grunt.loadNpmTasks("grunt-contrib-copy");
   grunt.loadNpmTasks("grunt-contrib-watch");
    
-  grunt.registerTask("default", [ "clean", "less", "uglify", "copy" ]);
+  grunt.registerTask("default", [
+    "clean",
+    "less:dist",
+    "compress:stylesheets/dist/css/bootstrap.css:stylesheets/dist/css/bootstrap.min.css",
+    "uglify",
+    "copy"
+  ]);
 
 };
