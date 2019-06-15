1be5d03ad0cc9069fcdd151a7d21d080e7db4b27
Mayhew Seavey <commander.boffo@gmail.com>
2019-06-14 18:20:48 -0700

    MM-894: Update cache with new group episode

--- javascripts/tv/services/seriesDetailService.js
+++ javascripts/tv/services/seriesDetailService.js
@@ -1,6 +1,6 @@
 angular.module('mediaMogulApp')
-  .service('SeriesDetailService', ['ArrayService', 'EpisodeService', '$q',
-    function (ArrayService, EpisodeService, $q) {
+  .service('SeriesDetailService', ['ArrayService', '$q', 'GroupService', '$injector',
+    function (ArrayService, $q, GroupService, $injector) {
       const self = this;
 
       let series;
@@ -10,6 +10,24 @@ angular.module('mediaMogulApp')
         return ArrayService.exists(series) && series_id === series.id;
       }
 
+      self.updateCacheWithGroupViewPayload = function(payload) {
+        if (!!series && series.id === payload.series_id) {
+          const episode = _.findWhere(episodes, {id: payload.episode_id});
+          const tv_group_id = payload.tv_group_id;
+          let groupEpisode = GroupService.getGroupEpisode(episode, tv_group_id);
+          if (!groupEpisode) {
+            groupEpisode = {
+              tv_group_id: tv_group_id
+            };
+            episode.groups.push(groupEpisode);
+          }
+          groupEpisode.watched = payload.watched;
+          groupEpisode.watched_date = payload.watched_date;
+          groupEpisode.skipped = payload.skipped;
+          groupEpisode.tv_group_episode_id = payload.tv_group_episode_id;
+        }
+      };
+
       self.getSeriesDetailInfo = function(series_id) {
         return $q(resolve => {
           if (alreadyHasSeries(series_id)) {
@@ -18,7 +36,7 @@ angular.module('mediaMogulApp')
               episodes: episodes
             });
           } else {
-            EpisodeService.getSeriesDetailInfo(series_id).then(response => {
+            $injector.get('EpisodeService').getSeriesDetailInfo(series_id).then(response => {
               series = response.series;
               episodes = response.episodes;
               resolve(response);
