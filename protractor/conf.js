7f807679b83835b49a81da8030852c5a322f2392
Mayhew Seavey <commander.boffo@gmail.com>
2019-07-01 15:53:37 -0700

    MM-768: Make protractor work with standalone server.

--- protractor/conf.js
+++ protractor/conf.js
@@ -1,6 +1,16 @@
 // conf.js
+
+// Requires global protractor installation. Run 'npm install -g protractor', then 'webdriver-manager update'.
+// Then use the global protractor location in WebStorm config:
+//    ~\AppData\Roaming\npm\node_modules\protractor
+
+const userDir = process.env.USERPROFILE;
+
 exports.config = {
   framework: 'jasmine',
-  seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
+  seleniumServerJar: userDir + '\\AppData\\Roaming\\npm\\node_modules\\' +
+    'protractor\\node_modules\\' +
+    'webdriver-manager\\selenium\\' +
+    'selenium-server-standalone-3.141.59.jar',
   specs: ['spec.js']
 };
