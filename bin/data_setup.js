dad2020a83e0ec02eb22604d7b4e471a43974e08
Mayhew Seavey <commander.boffo@gmail.com>
2019-07-01 18:04:53 -0700

    MM-768: Add new db restore task

---
+++ bin/data_setup.js
@@ -0,0 +1,21 @@
+const {spawn} = require('child_process');
+const assert = require('assert');
+
+assert(!!process.env.PGPASSFILE, 'Expected PGPASSFILE environment variable.');
+let backupDir = process.env.BACKUP_DIR_MM;
+assert(!!backupDir, 'Expected PGPASSFILE environment variable.');
+
+let args = [
+  '--host=localhost',
+  '--dbname=tv_e2e',
+  '--username=postgres',
+  '--clean',
+  '--format=custom',
+  '--verbose',
+  backupDir + '\\e2e\\empty_shows.dump'
+];
+const child = spawn('pg_restore', args);
+
+child.on('exit', code => {
+  console.log(`Exit code is: ${code}`);
+});
