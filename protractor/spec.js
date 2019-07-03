299dcb954bad46fd804491d5350593acf0f1a6af
Mayhew Seavey <commander.boffo@gmail.com>
2019-07-03 13:59:02 -0700

    MM-768: Simple mark watched test

--- protractor/spec.js
+++ protractor/spec.js
@@ -5,25 +5,48 @@ describe('MediaMogul basic tests', () => {
 
   const legionSpec = by.id('show_1');
 
+  function goToShow(id) {
+    const spec = by.id('show_' + id);
+    const showLink = element(spec);
+    expect(showLink).toBeDefined();
+    showLink.click();
+    expect(browser.getCurrentUrl()).toContain('show/' + id + '/episode');
+  }
+
+  function getSelectedEpisode() {
+    const selectedEpisode = element(by.className('selectedEpisodeTile'));
+    expect(selectedEpisode).toBeDefined();
+    return selectedEpisode;
+  }
+
+  function markWatched() {
+    const markWatchedButton = element(by.buttonText('Mark Watched'));
+    markWatchedButton.click();
+  }
+
   beforeAll(() => {
     browser.waitForAngularEnabled(false);
     browser.get(baseURL);
     let signInLink = element(by.id('sign_in'));
 
-    if (signInLink.isPresent() === true) {
-      signInLink.click();
+    signInLink.isDisplayed().then(is_displayed => {
 
-      const loginButtonExists = by.buttonText('Log in with Google');
-      browser.wait(() => browser.isElementPresent(loginButtonExists), 5000).then(() => {
-        const loginButton = element(loginButtonExists);
-        browser.sleep(4000);
-        loginButton.click();
-        browser.sleep(2000);
-        expect(browser.getCurrentUrl()).toBe(dashboardURL);
-      });
-    } else {
-      browser.get(dashboardURL);
-    }
+      if (is_displayed === true) {
+        signInLink.click();
+
+        const loginButtonExists = by.buttonText('Log in with Google');
+        browser.wait(() => browser.isElementPresent(loginButtonExists), 5000).then(() => {
+          const loginButton = element(loginButtonExists);
+          browser.sleep(4000);
+          loginButton.click();
+          browser.sleep(2000);
+          expect(browser.getCurrentUrl()).toBe(dashboardURL);
+        });
+      } else {
+        browser.get(dashboardURL);
+      }
+    });
+
   });
 
   beforeEach(() => {
@@ -37,12 +60,54 @@ describe('MediaMogul basic tests', () => {
   });
 
   it('click legion takes you to detail page', () => {
-    const legionShow = element(legionSpec);
-    legionShow.click();
-    expect(browser.getCurrentUrl()).toContain('show/1/episode');
+    goToShow(1);
 
     const seriesTitle = element(by.binding('ctrl.series.title'));
     expect(seriesTitle.getText()).toEqual('Legion');
   });
+
+  it('mark watched', () => {
+    goToShow(1);
+
+    const selectedEpisodeTile = getSelectedEpisode();
+    expect(selectedEpisodeTile.getAttribute('class')).toContain('tile-ready');
+
+    getEpisodeInfo(selectedEpisodeTile).then(episodeInfo => {
+      expect(episodeInfo.season).toEqual(2);
+      expect(episodeInfo.episode).toEqual(1);
+
+      markWatched();
+
+      expect(selectedEpisodeTile.getAttribute('class')).toContain('tile-watched');
+
+      browser.sleep(1000);
+
+      const newlySelected = getSelectedEpisode();
+      getEpisodeInfo(newlySelected).then(newEpisode => {
+        expect(newEpisode.season).toEqual(2);
+        expect(newEpisode.episode).toEqual(2);
+      });
+    });
+  });
+
+  function getEpisodeInfo(episodeTile) {
+    return new Promise(resolve => {
+      episodeTile.element(by.className('episodeTileNumber')).getText().then(episodeNumber => {
+
+        expect(episodeNumber).toContain('Season');
+        expect(episodeNumber).toContain('Episode');
+
+        const splits = episodeNumber.toString().split(' ');
+        const currentSeason = parseInt(splits[1]);
+        const currentEpisode = parseInt(splits[3]);
+
+        resolve({
+          season: currentSeason,
+          episode: currentEpisode
+        });
+      });
+    });
+
+  }
 
 });
