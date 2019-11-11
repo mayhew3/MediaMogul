1b6acf9531138f9fa6d749257524d41fd095ba3a
Mayhew Seavey <commander.boffo@gmail.com>
2019-11-11 12:21:27 -0800

    MM-858: Got child groups to be marked watched. Still not using return values in UI, but on reload, it is updated.

--- controllers/error_handler.js
+++ controllers/error_handler.js
@@ -2,5 +2,6 @@
 exports.throwError = function(err, consoleMsg, response) {
   response.status(500);
   response.json({ error: consoleMsg });
-  throw new Error(consoleMsg + ': ' + err.message);
+  console.error(consoleMsg);
+  throw err;
 };
