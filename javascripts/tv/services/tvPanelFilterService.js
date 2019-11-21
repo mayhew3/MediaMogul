bfe9dedf0f4e5f88f82da7de56a6545606c19d06
Mayhew Seavey <commander.boffo@gmail.com>
2019-11-20 23:13:32 -0800

    MM-910: Hide filter button, and don't override filter service if a panel is loaded that doesn't have filters.

--- javascripts/tv/services/tvPanelFilterService.js
+++ javascripts/tv/services/tvPanelFilterService.js
@@ -9,11 +9,13 @@ function TVPanelFilterService() {
   self.page_number = null;
 
   self.registerPanel = function(incoming_panel_id, incoming_filters, incoming_page_number) {
-    if (self.panel_id !== incoming_panel_id) {
-      self.filters = incoming_filters;
-      self.page_number = incoming_page_number;
+    if (!!incoming_filters && incoming_filters.length > 0) {
+      if (self.panel_id !== incoming_panel_id) {
+        self.filters = incoming_filters;
+        self.page_number = incoming_page_number;
+      }
+      self.panel_id = incoming_panel_id;
     }
-    self.panel_id = incoming_panel_id;
   };
 
 }
