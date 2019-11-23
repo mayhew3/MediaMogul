b9884e9ed5747164e49d91f7efc35a1e3b1e9ff9
Mayhew Seavey <commander.boffo@gmail.com>
2019-11-22 16:27:29 -0800

    MM-884: Allow any users to flag. Hide Favorite button if already favorite.

--- javascripts/tv/modifyPosterController.js
+++ javascripts/tv/modifyPosterController.js
@@ -28,14 +28,14 @@ angular.module('mediaMogulApp')
         });
       };
 
-      function hasChangedFromPrevious() {
+      self.hasChangedFromPrevious = function() {
         return !self.previous_poster ||
           self.tvdb_poster.tvdb_poster_id !== self.previous_poster.tvdb_poster_id;
-      }
+      };
  
       function addOrUpdatePoster() {
         return $q(resolve => {
-          if (hasChangedFromPrevious()) {
+          if (self.hasChangedFromPrevious()) {
             if (!!self.series.my_poster) {
               EpisodeService.updateMyPoster(self.series.my_poster.id, self.tvdb_poster.tvdb_poster_id).then(() => {
                 const myPoster = self.series.my_poster;
