6e94246671de9ee30980aa695990ad27ccde9b05
mayhew3 <commander.boffo@gmail.com>
2020-03-22 16:28:59 -0700

    MM-1008: Display series-level approvals

--- javascripts/admin/tvdbSeriesApprovalController.js
+++ javascripts/admin/tvdbSeriesApprovalController.js
@@ -6,9 +6,19 @@ angular.module('mediaMogulApp')
       self.LockService = LockService;
       self.TVDBApprovalService = TVDBApprovalService;
 
-      self.getSeriesNeedingApproval = function() {
-        const listObj = [];
-        const episodes = self.TVDBApprovalService.episodesWithNeededApproval;
+      self.seriesObjs = [];
+
+      function formatDateObjectForDisplay(dateObject) {
+        const options = {
+          year: "numeric", month: "2-digit",
+          day: "2-digit", timeZone: "America/Los_Angeles"
+        };
+
+        return dateObject === null ? null :
+          dateObject.toLocaleDateString("en-US", options);
+      }
+
+      function updateLocalSeriesList(episodes) {
         if (episodes.length > 0) {
           const groupedBySeries = _.groupBy(episodes);
           for (const seriesId in groupedBySeries) {
@@ -16,17 +26,40 @@ angular.module('mediaMogulApp')
               const episodes = groupedBySeries[seriesId];
               const firstEp = episodes[0];
 
+              const dates = _.map(episodes, episode => new Date(episode.date_added));
+              const dateAdded = _.max(dates);
+              const airTimes = _.map(episodes, episode => new Date(episode.air_time));
+              const airTime = _.min(airTimes);
+
               const seriesObj = {
                 series_title: firstEp.series_title,
                 series_id: firstEp.series_id,
-                episode_count: episodes.length
+                episodes: episodes,
+                lastAdded: formatDateObjectForDisplay(dateAdded),
+                firstAirTime: formatDateObjectForDisplay(airTime)
               };
-              listObj.push(seriesObj);
+              self.seriesObjs.push(seriesObj);
             }
           }
         }
+      }
 
-        return listObj;
+      self.getApproveButtonLabel = function(seriesObj) {
+        if (seriesObj.episodes.length === 1) {
+          return 'Approve';
+        } else {
+          return 'Approve All';
+        }
       };
+
+      self.getRejectButtonLabel = function(seriesObj) {
+        if (seriesObj.episodes.length === 1) {
+          return 'Reject';
+        } else {
+          return 'Reject All';
+        }
+      };
+
+      TVDBApprovalService.addListener(updateLocalSeriesList);
     }
   ]);
