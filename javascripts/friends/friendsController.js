a5dda525b9cd06c1e7f2126987cbc4b9bca1d19f
mayhew3 <commander.boffo@gmail.com>
2020-06-04 16:15:36 -0700

    MM-861: Cleanup

--- javascripts/friends/friendsController.js
+++ javascripts/friends/friendsController.js
@@ -8,26 +8,6 @@ angular.module('mediaMogulApp')
 
       NavHelperService.changeSelectedNav('Friends');
 
-      function hasSentPendingRequest(person) {
-        const matching = FriendService.getFriendship(person);
-        return !!matching && (matching.status === 'pending' || matching.status === 'ignored');
-      }
-
-      function hasReceivedPendingRequest(person) {
-        const matching = FriendService.getFriendshipRequest(person);
-        return !!matching && matching.status === 'pending';
-      }
-
-      function hasIgnoredRequest(person) {
-        const matching = FriendService.getFriendshipRequest(person);
-        return !!matching && matching.status === 'ignored';
-      }
-
-      function isFriendsWith(person) {
-        const matching = FriendService.getFriendship(person);
-        return !!matching && matching.status === 'approved';
-      }
-
       self.showIgnoreButton = function(person) {
         return hasReceivedPendingRequest(person) && !isFriendsWith(person) && !hasSentPendingRequest(person);
       };
@@ -76,34 +56,6 @@ angular.module('mediaMogulApp')
         return !hasSentPendingRequest(person) && !hasReceivedPendingRequest(person) && hasIgnoredRequest(person);
       };
 
-      self.getButtonText = function(person) {
-        if (isFriendsWith(person)) {
-          return "Remove";
-        } else if (hasSentPendingRequest(person)) {
-          return "Sent";
-        } else if (hasReceivedPendingRequest(person)) {
-          return "Approve";
-        } else if (hasIgnoredRequest(person)) {
-          return "Un-Ignore";
-        } else {
-          return "Add";
-        }
-      };
-
-      self.getButtonClass = function(person) {
-        if (isFriendsWith(person)) {
-          return "btn-danger";
-        } else if (hasSentPendingRequest(person)) {
-          return "btn-info";
-        } else if (hasReceivedPendingRequest(person)) {
-          return "btn-tv-primary";
-        } else if (hasIgnoredRequest(person)) {
-          return "btn-warning";
-        } else {
-          return "btn-default";
-        }
-      }
-
       self.getPotentialFriends = function() {
         return _.filter(PersonService.persons, potentialFriendsFilter);
       }
@@ -124,18 +76,24 @@ angular.module('mediaMogulApp')
             person.user_role !== 'guest'));
       }
 
-      self.handleClick = async function(person) {
-        if (isFriendsWith(person)) {
-          FriendService.removeFriend(person);
-        } else if (hasSentPendingRequest(person)) {
-          FriendService.unsendRequest(person);
-        } else if (hasReceivedPendingRequest(person)) {
-          FriendService.approveRequest(person);
-        } else if (hasIgnoredRequest(person)) {
-          FriendService.unIgnoreRequest(person);
-        } else {
-          FriendService.sendRequest(person);
-        }
+      function hasSentPendingRequest(person) {
+        const matching = FriendService.getFriendship(person);
+        return !!matching && (matching.status === 'pending' || matching.status === 'ignored');
+      }
+
+      function hasReceivedPendingRequest(person) {
+        const matching = FriendService.getFriendshipRequest(person);
+        return !!matching && matching.status === 'pending';
+      }
+
+      function hasIgnoredRequest(person) {
+        const matching = FriendService.getFriendshipRequest(person);
+        return !!matching && matching.status === 'ignored';
+      }
+
+      function isFriendsWith(person) {
+        const matching = FriendService.getFriendship(person);
+        return !!matching && matching.status === 'approved';
       }
 
     }
