247b95226b9e1e3669a089a92890cf61c8191272
mayhew3 <commander.boffo@gmail.com>
2020-06-05 16:11:16 -0700

    Trying to get karma set up

---
+++ javascripts/services/arrayService.spec.js
@@ -0,0 +1,13 @@
+describe('ArrayService', function() {
+
+  let ArrayService;
+
+  beforeEach(inject(function(_ArrayService_) {
+    ArrayService = _ArrayService_;
+  }));
+
+  it('should exist', function() {
+    expect(ArrayService).toBeDefined();
+  });
+
+});
