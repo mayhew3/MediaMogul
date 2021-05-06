71f1e4e98c42519d9a168d1ed72e5c7855119cbf
Mayhew Seavey <commander.boffo@gmail.com>
2021-05-06 14:41:06 -0700

    Back to HS256
    Seeing too many weird things in production with the new Auth. Not worth it.

--- javascripts/env.js
+++ javascripts/env.js
@@ -2,6 +2,6 @@
   window.__env = window.__env || {};
 
   window.__env.auth0_domain = 'mayhew3.auth0.com';
-  window.__env.auth0_client = 'HY2lTrNdFc6HDrTlSoKZNL0EriSi0dnW';
+  window.__env.auth0_client = 'QdwQv7LcXgmiUpYhXnTYyGQsXie2UQNb';
 
 }(this));
